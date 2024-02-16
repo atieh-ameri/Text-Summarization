233df5c6dfb42bb9a6c4ee86be5903f038cd6351
atieh-ameri <atieh.ameri@gmail.com>
2024-02-16 16:29:50 -0600

    requirments and installation done

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 REPO_NAME = "Text_Summarization"
 AUTHOR_USER_NAME = "atieh_ameri"
-SRC_REPO= "textSummerizer"
+SRC_REPO= "textSummarizer"
 AUTHOR_EMAIL = "ATIEH.AMERI@GMAIL.COM"
 
 setuptools.setup(
